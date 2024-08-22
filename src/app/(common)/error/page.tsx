150d8c24f955bb621a1e0f4da5ea123cdcde66a8
sutigit <panyanilben@gmail.com>
2024-08-22 12:55:48 +0300

    delete go back button

--- src/app/(common)/error/page.tsx
+++ src/app/(common)/error/page.tsx
@@ -5,9 +5,6 @@ export default function ErrorPage() {
         <main className="h-screen w-full flex flex-col gap-5 justify-center items-center ">
             <h1 className="text-3xl font-bold">Oh no! Something went wrong...</h1>
             <p className="text-xl font-medium">Sorry about it! Could you please try again?</p>
-
-            {/* TODO: actually take this to the previous page */}
-            <Button className="bg-pink text-black font-medium">Go Back</Button>
         </main>
     );
 }
